9308e03ecb6788610513b1cacef3cf14bae62da4
pizza4chezze <oogrusha@gmail.com>
2024-05-25 17:15:01 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using ReflectionWPFApp.ViewModels;
+using static System.Net.Mime.MediaTypeNames;
+using System.Windows.Controls;
+using System.Windows.Media.Media3D;
+
+using System.Windows;
+
+< Window x: Class = "ReflectionWPFApp.Views.MainWindow"
+        xmlns = "http://schemas.microsoft.com/winfx/2006/xaml/presentation"
+        xmlns: x = "http://schemas.microsoft.com/winfx/2006/xaml"
+        xmlns: d = "http://schemas.microsoft.com/expression/blend/2008"
+        xmlns: mc = "http://schemas.openxmlformats.org/markup-compatibility/2006"
+        xmlns: local = "clr-namespace:ReflectionWPFApp.Views"
+        xmlns: viewModel = "clr-namespace:ReflectionWPFApp.ViewModels"
+        mc: Ignorable = "d"
+        Title = "Reflection WPF App" Height = "450" Width = "800" >
+    < Window.DataContext >
+        < viewModel:MainViewModel />
+    </ Window.DataContext >
+    < Grid >
+        < StackPanel Margin = "10" >
+            < TextBox Text = "{Binding LibraryPath, UpdateSourceTrigger=PropertyChanged}" Width = "300" Margin = "5" PlaceholderText = "Enter path to the library" />
+            < Button Content = "Load Library" Command = "{Binding LoadLibraryCommand}" Width = "100" Margin = "5" />
+            < ComboBox ItemsSource = "{Binding Types}" SelectedItem = "{Binding SelectedType}" DisplayMemberPath = "Name" Width = "300" Margin = "5" />
+            < ComboBox ItemsSource = "{Binding Methods}" SelectedItem = "{Binding SelectedMethod}" DisplayMemberPath = "Name" Width = "300" Margin = "5" />
+            < ItemsControl ItemsSource = "{Binding Parameters}" >
+                < ItemsControl.ItemTemplate >
+                    < DataTemplate >
+                        < TextBox Text = "{Binding}" Width = "200" Margin = "5" />
+                    </ DataTemplate >
+                </ ItemsControl.ItemTemplate >
+            </ ItemsControl >
+            < Button Content = "Execute Method" Command = "{Binding ExecuteMethodCommand}" Width = "100" Margin = "5" />
+        </ StackPanel >
+    </ Grid >
+</ Window >
---
+++ ViewModels/MainViewModels.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Windows.Input;
+
+namespace ReflectionWPFApp.ViewModels
+{
+    public class MainViewModel : BaseViewModel
+    {
+        private string _libraryPath;
+        private Type _selectedType;
+        private MethodInfo _selectedMethod;
+        private object _selectedObject;
+        private ObservableCollection<MethodInfo> _methods;
+        private ObservableCollection<Type> _types;
+        private ObservableCollection<object> _parameters;
+
+        public string LibraryPath
+        {
+            get => _libraryPath;
+            set
+            {
+                _libraryPath = value;
+                OnPropertyChanged();
+                LoadTypes();
+            }
+        }
+
+        public ObservableCollection<Type> Types
+        {
+            get => _types;
+            set
+            {
+                _types = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public Type SelectedType
+        {
+            get => _selectedType;
+            set
+            {
+                _selectedType = value;
+                OnPropertyChanged();
+                LoadMethods();
+            }
+        }
+
+        public ObservableCollection<MethodInfo> Methods
+        {
+            get => _methods;
+            set
+            {
+                _methods = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MethodInfo SelectedMethod
+        {
+            get => _selectedMethod;
+            set
+            {
+                _selectedMethod = value;
+                OnPropertyChanged();
+                LoadParameters();
+            }
+        }
+
+        public ObservableCollection<object> Parameters
+        {
+            get => _parameters;
+            set
+            {
+                _parameters = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand ExecuteMethodCommand { get; }
+
+        public MainViewModel()
+        {
+            Types = new ObservableCollection<Type>();
+            Methods = new ObservableCollection<MethodInfo>();
+            Parameters = new ObservableCollection<object>();
+            ExecuteMethodCommand = new RelayCommand(ExecuteMethod);
+        }
+
+        private void LoadTypes()
+        {
+            if (File.Exists(LibraryPath))
+            {
+                Assembly assembly = Assembly.LoadFrom(LibraryPath);
+                var types = assembly.GetTypes().Where(t => t.IsSubclassOf(typeof(AnimalLibrary.LivingCreature)));
+                Types.Clear();
+                foreach (var type in types)
+                {
+                    Types.Add(type);
+                }
+            }
+        }
+
+        private void LoadMethods()
+        {
+            if (SelectedType != null)
+            {
+                Methods.Clear();
+                var methods = SelectedType.GetMethods(BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly);
+                foreach (var method in methods)
+                {
+                    Methods.Add(method);
+                }
+            }
+        }
+
+        private void LoadParameters()
+        {
+            if (SelectedMethod != null)
+            {
+                Parameters.Clear();
+                foreach (var parameter in SelectedMethod.GetParameters())
+                {
+                    Parameters.Add(Activator.CreateInstance(parameter.ParameterType));
+                }
+            }
+        }
+
+        private void ExecuteMethod()
+        {
+            if (SelectedMethod != null && SelectedType != null)
+            {
+                _selectedObject = Activator.CreateInstance(SelectedType);
+                SelectedMethod.Invoke(_selectedObject, Parameters.ToArray());
+            }
+        }
+    }
+}
+
